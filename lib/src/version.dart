dfb00cb5c9cb7977d1c2c16a2770a51e49c3eab6
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-01-26 22:21:57 +0100

    fix: version

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,7 +1,7 @@
 /// This library contains Unleash SDK constants used by this package.
 library version;
 
-const String version = '0.3.0';
+const String version = '0.4.0';
 
 const String name = 'unleash-client-dart';
 
